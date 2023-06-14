34a7b5ae7f0b3333cc8f03fac5cadbb9760673fe
NikitaGrimes <nikita.rick@gmail.com>
2023-06-14 12:34:59 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription, BehaviorSubject, interval} from 'rxjs';
+import { StopWatch } from './models/stop-watch';
+import { TimerService } from './services/timer.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'stopwatch';
+export class AppComponent implements OnDestroy{
+  time: StopWatch = new StopWatch(0);
+  timerSubscription?: Subscription;
+  isTimerRunning: boolean = false;
+  dbClickSubject?: BehaviorSubject<number>;
+  
+  constructor(private timerService: TimerService){
+
+  }
+
+  ngOnDestroy(): void {
+    this.timerSubscription?.unsubscribe();
+  }
+
+  startWatching(): void{
+    if (this.isTimerRunning)
+      return;
+
+    this.timerSubscription = this.timerService.getTimer().subscribe(second => this.time.seconds = second)
+    this.isTimerRunning = true;
+  }
+
+  stopWatching(): void{
+    this.timerSubscription?.unsubscribe();
+    this.timerService.clear();
+    this.isTimerRunning = false;
+  }
+
+  resetWatching(): void{
+    this.timerSubscription?.unsubscribe();
+    this.timerService.clear();
+    this.timerSubscription = this.timerService.getTimer().subscribe(second => this.time.seconds = second)
+    this.isTimerRunning = true;
+  }
+
+  waitWatching(): void{
+    if(!this.dbClickSubject){
+      this.dbClickSubject = new BehaviorSubject<number>(Date.now());
+      return;
+    }
+
+    let timeNow = Date.now();
+    this.dbClickSubject.subscribe(lastTime => {
+      if ((timeNow - lastTime) < 300){
+        this.timerSubscription?.unsubscribe();
+        this.isTimerRunning = false;
+      }
+    }).unsubscribe();
+    this.dbClickSubject.next(timeNow);
+  }
 }
---
+++ src/app/services/timer.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimerService {
+  private lastmSecond: number = 0;
+  constructor() { }
+
+  getTimer(): Observable<number>{
+    return new Observable<number>(subscriber => {
+      subscriber.next(this.lastmSecond);
+      const timer = setInterval(() => subscriber.next(++this.lastmSecond), 100)
+      
+      return{
+        unsubscribe() {
+          clearInterval(timer);
+        }
+      }
+    })
+  }
+
+  clear(): void{
+    this.lastmSecond = 0;
+  }
+}
