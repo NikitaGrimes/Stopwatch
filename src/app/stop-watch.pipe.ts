d8e0566c2f48515cb1a53e6f4ed27f23e286a189
NikitaGrimes <nikita.rick@gmail.com>
2023-06-15 13:07:56 +0300

    i am close

--- src/app/stop-watch.pipe.ts
+++ src/app/stop-watch.pipe.ts
@@ -7,10 +7,10 @@ export class StopWatchPipe implements PipeTransform {
 
   transform(value: number | null): string {
     if(value !== null){
-      let mSecond = (value % 10).toString()
-      let second = this.normolizeNumber((Math.floor(value / 10)) % 60);
-      let minute = this.normolizeNumber((Math.floor(value / 600)) % 60);
-      let hour = this.normolizeNumber(Math.floor(value / 36000));
+      const mSecond = (value % 10).toString()
+      const second = this.normolizeNumber((Math.floor(value / 10)) % 60);
+      const minute = this.normolizeNumber((Math.floor(value / 600)) % 60);
+      const hour = this.normolizeNumber(Math.floor(value / 36000));
       return `${hour}:${minute}:${second}:${mSecond}`;
     }
 
