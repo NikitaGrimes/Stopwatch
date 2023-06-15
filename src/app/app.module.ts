36144e304797b79ab61dd7938380034db8927ae5
NikitaGrimes <nikita.rick@gmail.com>
2023-06-15 14:12:47 +0300

    finish2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { StopWatchPipe } from './stop-watch.pipe';
+import { StopWatchPipe } from './pipes/stop-watch.pipe';
 
 @NgModule({
   declarations: [
