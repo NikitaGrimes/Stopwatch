7e4173aa3c3d30aa6b4284dbdd98a70e321dce7e
NikitaGrimes <nikita.rick@gmail.com>
2023-06-15 13:08:42 +0300

    i am close

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { BehaviorSubject, interval, map, of, pipe, startWith, switchMap } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { BehaviorSubject, interval, of, startWith, switchMap, map, Subscription, Subject, filter, timer } from 'rxjs';
 
 type Command = "start" | "stop" | "wait" | "reset";
 
@@ -8,30 +8,45 @@ type Command = "start" | "stop" | "wait" | "reset";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
-  time = 0;
-  timer$ = new BehaviorSubject<number>(this.time);
+export class AppComponent implements OnDestroy{
+  timer$ = new BehaviorSubject<number>(0);
   command$ = new BehaviorSubject<Command>("stop");
+  commandSubscribtion?: Subscription;
   
   constructor(){
-    this.command$.pipe(switchMap((command) => interval(100).pipe(map(_ => {
+    let currentTime = 0;
+    let waitTime = 0;
+    let lastClickTime = 0;
+    this.commandSubscribtion = this.command$.pipe(filter(command => {
+      const nowClickTime = Date.now();
+      if (command !== "wait" || (command === "wait" && ((nowClickTime - lastClickTime) < 300)))
+        return true;
+      lastClickTime = nowClickTime;
+      return false;
+    }), switchMap(command => {
       switch (command){
         case "start":
-          this.timer$.next(++this.time);
-          break;
-        case "reset":
-          this.time = 0;
-          this.timer$.next(++this.time);
-          this.command$.next("start");
-          break;
+          return interval(1000).pipe(map(value => ++value), startWith(0));
         case "stop":
-          this.command$.next("wait");
-          this.time = 0;
-          break;
+          currentTime += waitTime;
+          waitTime = 0;
+          return of(currentTime);
         case "wait":
-          
+           waitTime += currentTime;
+          return of(0);
+        case "reset":
+          waitTime = 0;
+          return interval(1000).pipe(map(value => ++value), startWith(0));
       }
-    }), startWith(this.timer$.next(this.time))))).subscribe();
+    })).subscribe(value => {
+      
+      currentTime = value;
+      this.timer$.next(waitTime + currentTime);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.commandSubscribtion?.unsubscribe();
   }
 
   startWatching(): void{
